02f5ee1912acfe0b1fe698130f48370b179b9b1f
Cicero Roberto Nobre de Oliveira Alcântara <cicero@ciandt.com>
2020-02-17 14:38:45 -0300

    Testing view list

--- src/components/unicorns/__tests__/UnicornView.js
+++ src/components/unicorns/__tests__/UnicornView.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 
 import UnicornView from './../UnicornView';
@@ -7,6 +7,39 @@ import store from './../../../store';
 
 jest.mock('../../ActionsUnicorn');
 const actions = require('../../ActionsUnicorn');
+
+const Mockedlist = [
+  {
+    _id: 1,
+    name: 'Unicorn 1',
+    age: 1,
+    colour: 'Colour 1'
+  },
+  {
+    _id: 2,
+    name: 'Unicorn 2',
+    age: 2,
+    colour: 'Colour 2'
+  },
+  {
+    _id: 3,
+    name: 'Unicorn 3',
+    age: 3,
+    colour: 'Colour 3'
+  },
+  {
+    _id: 4,
+    name: 'Unicorn 4',
+    age: 4,
+    colour: 'Colour 4'
+  },
+  {
+    _id: 5,
+    name: 'Unicorn 5',
+    age: 5,
+    colour: 'Colour 5'
+  },
+];
 
 describe("Unicorn view testing hooks", () => {
   
@@ -22,58 +55,13 @@ describe("Unicorn view testing hooks", () => {
     }
   }
 
-  beforeEach(() => {
-    store.dispatch({ type: 'RESET' })
-  });
+  it("[useEffect] = Get all and set list in reducer", async () => {
+    store.dispatch({ type: 'GET_SUCCESS', data: Mockedlist });
+    actions.getAllUnicorn.mockReturnValue(() => ({ type: 'GET_SUCCESS', data: Mockedlist }));
 
-  it("[useState] = The form is initialized with empty values", () => {
-    const { name, age, colour } = setup();
-  
-    expect(name.value).toBe("");
-    expect(age.value).toBe("");
-    expect(colour.value).toBe("");
-  });
-
-  it("[useState] = The form is updated with the guys data", () => {
-    const { name, age, colour } = setup();
-  
-    fireEvent.change(name, { target: { value: 'Happunzell' } });
-    fireEvent.change(age, { target: { value: 16 } });
-    fireEvent.change(colour, { target: { value: 'Dark Purple' } });
-  
-    expect(name.value).toBe("Happunzell");
-    expect(age.value).toBe("16");
-    expect(colour.value).toBe("Dark Purple");
-  });
-  
-  it("[useState] = Is not possible write letter to age field", () => {
-    const { age } = setup();
-  
-    fireEvent.change(age, { target: { value: "Dracula" } });
-  
-    expect(age.value).toBe("");
-  });
-
-  it("[useEffect] = Should set in state, when theres data on redux", async () => {
-    store.dispatch({ type: 'GET_BY_ID_SUCCESS', data: MockedUnicorn });
-    const { item } = store.getState().ReducerUnicorn;
-
-    const { name, age, colour } = setup();
+    setup();
     
-    expect(name.value).toBe(item.name);
-    expect(age.value).toBe(item.age.toString());
-    expect(colour.value).toBe(item.colour);
-  });
-
-  it("On post unicorn", async () => {
-    const { btnSubmit } = setup();
-
-    actions.postUnicorn.mockReturnValue(() => Promise.resolve());
-    actions.getAllUnicorn.mockReturnValue(() => Promise.resolve());
-
-    fireEvent.click(btnSubmit);
-    
-    await expect(actions.postUnicorn.mock.calls.length).toBe(1);
-    expect(actions.getAllUnicorn.mock.calls.length).toBe(1);
+    await expect(actions.getAllUnicorn.mock.calls.length).toBe(1);
+    expect(store.getState().ReducerUnicorn.list.length).toBe(5);
   });
 })
